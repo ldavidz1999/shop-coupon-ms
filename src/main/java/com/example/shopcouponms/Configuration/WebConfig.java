abf6f09bd8e44cc6859d39f71dc07419134296e8
ldavidz1999 <61469694+ldavidz1999@users.noreply.github.com>
2024-03-05 22:16:10 -0500

    Cors policy update

--- src/main/java/com/example/shopcouponms/Configuration/WebConfig.java
+++ src/main/java/com/example/shopcouponms/Configuration/WebConfig.java
@@ -6,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://shop-coupon-ms-production.up.railway.app/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
